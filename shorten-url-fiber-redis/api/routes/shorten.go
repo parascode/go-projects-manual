84dc54be18f8694eebcfa58364ed2308925e5955
parascode <jparas435@gmail.com>
2024-08-20 22:21:03 +0530

    implemented main.go and helper methods

---
+++ shorten-url-fiber-redis/api/routes/shorten.go
@@ -0,0 +1,47 @@
+package routes
+
+import (
+	"helpers"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type request struct{
+	URL				string				`json:"url"`
+	CustomShort		string				`json:"short"`
+	Expiry			time.Duration		`json:"expiry"`
+}
+
+type response struct{
+	URL					string				`json:"url"`
+	CustomShort			string				`json:"short"`
+	Expiry				time.Duration		`json:"expiry"`
+	XRateRemaining		int					`json:"rate_limit"`
+	XRateLimitReset		time.Duration		`json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error{
+	body := new(request)
+	
+	if err := c.BodyParser(&body); err != nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse JSON"})
+	}
+
+	//implementing rate limiting
+
+	// check if the input is an actual link
+	if !govalidator.IsURL(body.URL){
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid URL"})
+	}
+
+	// check for domain error
+
+	if !helpers.RemoveDomainError(body.URL){
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error":"trying to get into infinite loop"})
+	}
+
+	// enforce https, SSL
+
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
---
+++ shorten-url-fiber-redis/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes(app *fiber.App){
+	app.Get("/:url", routes.ResolveUrl)
+	app.Post("/api/v1", routes.ShortenUrl)
+}
+
+func main(){
+	err := godotenv.Load()
+
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	setupRoutes(app)
+
+	log.fatal(app.Listen(os.Getenv("APP_PORT")))
+
+}
